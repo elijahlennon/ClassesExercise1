a3369fcbd82b77cf9c8c3780d9186a8e41246dd3
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-12 22:55:51 -0500

    Completed Exercise

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elijahsCar = new Car();
+
+            elijahsCar.Make = "Hyudai";
+
+            elijahsCar.Model = "Sonata Hybrid";
+
+            elijahsCar.Year = 2014;
+
+            Console.WriteLine($"Elijah drives {elijahsCar.Make}, {elijahsCar.Model}, {elijahsCar.Year}.");
+        }
+        
+
+    }
+}
